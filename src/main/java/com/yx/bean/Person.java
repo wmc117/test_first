ca0d12d79f0656209dfe0415e4d39eb1d37623a7
wmc117 <957146590@qq.com>
2017-03-11 15:33:57 +0800

    first_commit

---
+++ src/main/java/com/yx/bean/Person.java
@@ -0,0 +1,39 @@
+package com.yx.bean;
+
+/**
+ * User: wmc
+ * Date: 17-3-11
+ */
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wmc
+  Date: 17-3-11
+  Time: 下午3:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
