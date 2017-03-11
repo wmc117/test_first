87464c7ab649c05d5abf23dacb2678f2b546cb1b
wmc117 <957146590@qq.com>
2017-03-11 16:07:30 +0800

    update

---
+++ src/main/java/com/yx/test/Test.java
@@ -0,0 +1,13 @@
+package com.yx.test;
+
+/**
+ * User: wmc
+ * Date: 17-3-11
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hello");
+        }
+    }
+}
